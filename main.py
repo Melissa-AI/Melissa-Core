2fc8950bbc18f7ddff7830e054f0e346a1e189a4
Shivam Mamgain <shivammamgain47@gmail.com>
2015-11-30 18:23:36 +0530

    Add requirements.txt

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pywapi
 import random
 import tweepy
 import json
+import pyaudio
 from tweepy import OAuthHandler
 import speech_recognition as sr
 from selenium import webdriver
@@ -30,9 +31,9 @@ def tts(message):
     if sys.platform == 'darwin':
         tts_engine = 'say'
         return os.system(tts_engine + ' ' + message)
-    elif sys.platform == 'linux':
+    elif sys.platform == 'linux2' or sys.platform == 'linux':
         tts_engine = 'espeak'
-        return os.system(tts_engine + ' ' + message)
+        return os.system(tts_engine + ' "' + message + '"')
 
 tts('Welcome ' + name + ', systems are now ready to run. How can I help you?')
 
@@ -56,20 +57,20 @@ def check_message(check):
         return False
 
 if check_message(['who','are', 'you']):
-    messages = ['I am Melissa, your lovely personal assistant.', 
-    'Melissa, didnt I tell you before?', 
+    messages = ['I am Melissa, your lovely personal assistant.',
+    'Melissa, didnt I tell you before?',
     'You ask that so many times! I am Melissa.']
     tts(random.choice(messages))
-    
+
 elif check_message(['who', 'am', 'I']):
     tts('You are ' + name + ', a brilliant person. I love you!')
-    
+
 elif check_message(['where', 'born']):
     tts('I was created by a magician named Tanay, in India, the magical land of himalayas.')
 
 elif check_message(['how', 'are', 'you']):
     tts('I am fine, thank you.')
-    
+
 elif check_message(['my', 'tweets']):
     # This needs formatting, not currently fit to be run.
     tts('Loading your tweets, ' + name)
@@ -87,7 +88,7 @@ elif check_message(['play', 'music']) or check_message(['music']):
                     music_list.append(os.path.join(root, filename))
         music_play = random.choice(music_list)
         return music_play
-    
+
     music_playing = mp3gen()
     tts("Now playing: " + music_playing)
     os.system("afplay " + music_playing)
@@ -96,20 +97,20 @@ elif check_message(['how', 'weather']):
     weather_com_result = pywapi.get_weather_from_weather_com(city_code)
     weather_result = "Weather.com says: It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "degree celcius now in " + city_name
     tts(weather_result)
-    
+
 elif check_message(['connect', 'proxy']):
     tts("Connecting to proxy server.")
     browser = webdriver.Firefox()
     browser.get('http://10.1.1.9:8090/httpclient.html')
-    
+
     id_number = browser.find_element_by_name('username')
     password = browser.find_element_by_name('password')
-    
+
     id_number.send_keys(proxy_username)
     password.send_keys(proxy_password)
-    
+
     browser.find_element_by_name('btnSubmit').click()
-    
+
 
 elif check_message(['bye']) or check_message(['goodbye']):
     tts('Goodbye!')
