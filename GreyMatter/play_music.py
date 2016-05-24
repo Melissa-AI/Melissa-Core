56fb0ed23db50c4449ecbcd3f7113a55a36fdf5b
neilnelson <nnelson@infowest.com>
2016-05-23 21:11:37 -0600

    Added sox capability.

--- GreyMatter/play_music.py
+++ GreyMatter/play_music.py
@@ -4,34 +4,56 @@ import random
 
 from SenseCells.tts import tts
 
+sox_file_types = ['.wav','.flac','ogg']
+music_listing = None
+
 def mp3gen(music_path):
     """
     This function finds all the mp3 files in a folder and it's subfolders and returns a list.
     """
-    music_list = []
+    global music_listing
+
+    if music_listing != None:
+        return
+
+    if sys.platform != 'darwin' \
+    and sys.platform != 'win32' \
+    and not sys.platform.startswith('linux'):
+        print "Music only enabled on darwin, win32, and linux."
+        return
+
+    music_listing = []
     for root, dirs, files in os.walk(music_path):
         for filename in files:
             if os.path.splitext(filename)[1] == ".mp3":
-                music_list.append(os.path.join(root, filename.lower()))
-    return music_list
+                if sys.platform == 'darwin':
+                    music_listing.append([
+                        'afplay',
+                        os.path.join(root, filename.lower())])
+                else:
+                    music_listing.append([
+                        'mpg123',
+                        os.path.join(root, filename.lower())])
+            elif os.path.splitext(filename)[1] in sox_file_types:
+                if sys.platform != 'darwin':
+                    music_listing.append([
+                        'play',
+                        os.path.join(root, filename.lower())])
 
-def music_player(file_name):
+def music_player(music_selection):
     """
     This function takes the name of a music file as an argument and plays it depending on the OS.
     """
-    if sys.platform == 'darwin':
-        player = "afplay '" + file_name + "'"
-        return os.system(player)
-    elif sys.platform == 'linux2' or sys.platform == 'linux' or sys.platform == 'win32':
-        player = "mpg123 '" + file_name + "'"
-        return os.system(player)
+    player = music_selection[0] + " '" + music_selection[1] + "'"
+    return os.system(player)
 
 def play_random(music_path):
     try:
-        music_listing = mp3gen(music_path)
+        mp3gen(music_path)
         music_playing = random.choice(music_listing)
-        tts("Now playing: " + music_playing)
+        tts("Now playing: " + music_playing[1])
         music_player(music_playing)
+
     except IndexError as e:
         tts('No music files found.')
         print("No music files found: {0}".format(e))
@@ -40,18 +62,20 @@ def play_specific_music(speech_text, music_path):
     words_of_message = speech_text.split()
     words_of_message.remove('play')
     cleaned_message = ' '.join(words_of_message)
-    music_listing = mp3gen(music_path)
+    mp3gen(music_path)
 
     for i in range(0, len(music_listing)):
-        if cleaned_message in music_listing[i]:
+        if cleaned_message in music_listing[i][1]:
             music_player(music_listing[i])
 
 def play_shuffle(music_path):
     try:
-        music_listing = mp3gen(music_path)
+        mp3gen(music_path)
         random.shuffle(music_listing)
         for i in range(0, len(music_listing)):
             music_player(music_listing[i])
+
     except IndexError as e:
         tts('No music files found.')
         print("No music files found: {0}".format(e))
+
