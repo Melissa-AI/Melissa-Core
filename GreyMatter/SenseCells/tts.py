2a9bee6347a2eecd54d7d953bf703f222ab3f95f
Tanay Pant <tanay1337@gmail.com>
2016-05-28 09:58:10 +0530

    Change os.system instances to subprocess.call

--- GreyMatter/SenseCells/tts.py
+++ GreyMatter/SenseCells/tts.py
@@ -1,5 +1,5 @@
-import os
 import sys
+import subprocess
 
 def tts(message):
     """
@@ -7,7 +7,7 @@ def tts(message):
     """
     if sys.platform == 'darwin':
         tts_engine = 'say'
-        return os.system(tts_engine + ' ' + message)
+        return subprocess.call([tts_engine, message])
     elif sys.platform == 'linux2' or sys.platform == 'linux' or sys.platform == 'win32':
         tts_engine = 'espeak'
-        return os.system(tts_engine + ' "' + message + '"')
+        return subprocess.call([tts_engine, message])
--- GreyMatter/lighting.py
+++ GreyMatter/lighting.py
@@ -1,22 +1,22 @@
-import os
+import subprocess
 
 from SenseCells.tts import tts
 
 def very_dark():
-    os.system('blink1-tool --white')
+    subprocess.call(['blink1-tool', '--white'])
     tts('Better now?')
 
 def feeling_angry():
-    os.system('blink1-tool --cyan')
+    subprocess.call(['blink1-tool', '--cyan'])
     tts('Calm down dear!')
 
 def feeling_creative():
-    os.system('blink1-tool --magenta')
+    subprocess.call(['blink1-tool', '--magenta'])
     tts('So good to hear that!')
 
 def feeling_lazy():
-    os.system('blink1-tool --yellow')
+    subprocess.call(['blink1-tool', '--yellow'])
     tts('Rise and shine dear!')
 
 def turn_off():
-    os.system('blink1-tool --off')
+    subprocess.call(['blink1-tool', '--off'])
