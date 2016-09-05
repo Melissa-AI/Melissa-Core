be84373b6c05c679a333ba9331dd970e3f7e0886
Tanay Pant <tanay1337@gmail.com>
2016-09-05 23:11:46 +0530

    Add capability to list IP address

---
+++ melissa/actions/ip_address.py
@@ -0,0 +1,24 @@
+import re
+
+from netifaces import interfaces, ifaddresses, AF_INET
+
+# Melissa
+from melissa.tts import tts
+
+WORDS = {'ip_address': {'groups': ['ip', ['ip', 'address'], ['network', 'address']]}}
+
+def ip_address(text):
+    tts("Here are my available I.P. addresses.")
+    for ifaceName in interfaces():
+        addresses = [
+            i['addr'] for i in
+            ifaddresses(ifaceName).setdefault(
+                AF_INET, [{'addr': None}])]
+        if None in addresses:
+            addresses.remove(None)
+        if addresses and ifaceName != "lo":
+            updated_addresses = [re.sub(r"\.", r" dot ", address)
+                                 for address in addresses]
+            tts('%s: %s' % ("interface: " + ifaceName +", I.P. Address ", ', '.join(updated_addresses)))
+
+    tts("Those are all my I.P. addresses.")
