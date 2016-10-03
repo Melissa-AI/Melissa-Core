a6cef39247bcfa3403af3a61bd10f1986efb0616
Anurag Sharma <anssharma61@gmail.com>
2016-10-03 20:14:13 +0530

    Repeat action

---
+++ melissa/actions/repeat.py
@@ -0,0 +1,11 @@
+# Melissa
+from melissa import profile
+from melissa.tts import tts
+
+
+WORDS = {'repeat_text': {'groups': ['repeat', 'say']}
+        }
+
+def repeat_text(text):
+    text = text.split(' ', 1)[1]
+    tts(text)
