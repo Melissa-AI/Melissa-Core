8223f22760916059b7b0c7de7b88ace40f728653
Matt Kilens <kilens@stolaf.edu>
2016-10-06 11:55:37 -0500

    Added what_is_date() and what_is_time()

--- melissa/actions/tell_time.py
+++ melissa/actions/tell_time.py
@@ -3,8 +3,20 @@ from datetime import datetime
 # Melissa
 from melissa.tts import tts
 
-WORDS = {'what_is_time': {'groups': ['time']}}
+WORDS = {
+  'what_is_time': {'groups': ['time']},
+  'what_is_date': {'groups': ['date']},
+  'what_is_day': {'groups': ['day']}
+}
 
 
 def what_is_time(text):
     tts("The time is " + datetime.strftime(datetime.now(), '%H:%M:%S'))
+
+
+def what_is_date(text):
+    tts("The date is " + datetime.strftime(datetime.now(), '%d/%m/%Y'))
+
+
+def what_is_day(text):
+    tts("The day is " + datetime.strftime(datetime.now(), '%A'))
