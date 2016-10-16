94818200d3e17ed754f118991ea3ed05c553bd95
rachmadaniHaryono <foreturiga@gmail.com>
2016-10-16 17:11:47 +0800

    create tempfile for testing.

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,4 +1,8 @@
 """test profile module."""
+from shutil import copyfile
+import json
+import os
+import random
 try:  # py3
     from unittest import mock
 except ImportError:  # py2
@@ -31,3 +35,29 @@ def test_import_with_mock_profile_populator():
         with pytest.raises(IOError):
             from melissa import profile  # NOQA
         mock_profile_populator.assert_called_once_with()
+
+
+def test_import_with_temp_file():
+    """test importing but create temp file."""
+    with mock.patch('melissa.profile_populator.profile_populator') \
+            as mock_profile_populator:
+        random_int = random.randint(0, 9)
+        json_file = 'profile.json'
+        bak_file = json_file + '.bak'
+        json_file_exist = False
+        try:
+            # backup the file
+            if os.path.isfile(json_file):
+                json_file_exist = True
+                copyfile(json_file, bak_file)
+            with open(json_file, 'w') as f:
+                json.dump(random_int, f)
+
+            from melissa import profile
+            assert profile.data == random_int
+        finally:
+            os.remove(json_file)
+            # restore the backup
+            if json_file_exist:
+                copyfile(json_file, bak_file)
+    assert not mock_profile_populator.called
